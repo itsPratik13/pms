5cef62f863352c7a0f43aea1e47634e704d3c105
Pratik Survase <survasepratik1304@gmail.com>
2026-02-02 13:31:51 +0530

    sidebar

--- client/app/components/sidebar.tsx
+++ client/app/components/sidebar.tsx
@@ -1,9 +1,12 @@
 "use client"
-import React from 'react'
+import React, { useState } from 'react'
 
 const Sidebar = () => {
+    const[showProjects,setShowProjects]=useState(true);
+    const[showPriority,setShowPriority]=useState(true);
+
   return (
-    <div>Sidebar</div>
+    <div className=' bg-transparent min-h-screen w-80 flex flex-col justify-between z-20 shadow-2xl dark:bg-neutral-900  '></div>
   )
 }
 
