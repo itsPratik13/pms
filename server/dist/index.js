857c2c2d5e19b3433d8f4231ba6bdaa6a8566510
Pratik Survase <survasepratik1304@gmail.com>
2026-02-09 21:22:08 +0530

    backend setup

---
+++ server/dist/index.js
@@ -0,0 +1,10 @@
+import express from "express";
+const app = express();
+app.get("/", (req, res) => {
+    console.log("Working");
+});
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+    console.log("Server is running");
+});
+//# sourceMappingURL=index.js.map
